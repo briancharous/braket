2385fbb45b30e2eb4d7d360dd72da99f6504fa9d
Brian Charous <brian.charous@gmail.com>
2015-04-16 17:36:35 -0500

    change port for heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function(request, response) {
     response.sendFile(__dirname + '/static/home.html');
 });
 
-http.listen(5000, function() {
+http.listen(80, function() {
     console.log('listening on *:5000');
 });
 
